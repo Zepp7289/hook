4d7422680a56cbcb032d0126581cb7adbc9e8a96
Zepp7289 <zepp7289@gmail.com>
2026-09-24 12:26:46 +0800

    fix typo

--- hook.c
+++ hook.c
@@ -183,7 +183,7 @@ static void after_reinstall_suspended_bps(hook_fargs1_t *args, void *udata) {
             pr_info("trace stop\n");
             trace_task = NULL;
         } else {
-            if (regs->pc >= (uint64_t)segment_addr + segment_func_offset_next && regs->pc <= (uint64_t)segment_addr + segment_func_offset_next) {
+            if (regs->pc >= (uint64_t)segment_addr + segment_func_offset && regs->pc <= (uint64_t)segment_addr + segment_func_offset_next) {
                 pr_info("trace pc: %px\n", regs->pc);
             }
             user_enable_single_step_ptr(current);
